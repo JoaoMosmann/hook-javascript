d4ca8a5a78464a2619e58de79d86a6430e7a4d0f
Fernando França <fefranca@doubleleft.com>
2014-05-26 14:14:54 -0300

    update public documentation

---
+++ api.js
@@ -0,0 +1,42 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Backbone.DLCollection",
+        "Backbone.DLModel",
+        "Cordova",
+        "DL.Auth",
+        "DL.Channel.SSE",
+        "DL.Channel.WEBSOCKETS",
+        "DL.Client",
+        "DL.Collection",
+        "DL.Events",
+        "DL.Files",
+        "DL.Iterable",
+        "DL.KeyValues",
+        "DL.Pagination",
+        "DL.Plugin.Cordova.PushNotification",
+        "DL.PluginManager",
+        "DL.System"
+    ],
+    "modules": [
+        "Backbone",
+        "DL",
+        "DL.Plugin"
+    ],
+    "allModules": [
+        {
+            "displayName": "Backbone",
+            "name": "Backbone"
+        },
+        {
+            "displayName": "DL",
+            "name": "DL",
+            "description": "Deals with user registration/authentication"
+        },
+        {
+            "displayName": "DL.Plugin",
+            "name": "DL.Plugin"
+        }
+    ]
+} };
+});
